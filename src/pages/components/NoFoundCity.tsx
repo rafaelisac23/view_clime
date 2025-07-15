dc3a95a0b9aba9088ddbcaa5600edb268ab8b3b0
rafael isac <rafael.darrigo@gmail.com>
2025-07-15 11:05:32 -0300

    create_initial_req

---
+++ src/pages/components/NoFoundCity.tsx
@@ -0,0 +1,10 @@
+export const NoFoundCity = () => {
+  return (
+    <div className="w-[300px] h-[250px] border border-gray-500 rounded-md flex flex-col items-center justify-center">
+      <h1 className="text-center text-2xl font-semibold">
+        Cidade não encontrada !
+      </h1>
+      <h2 className="text-center font-semibold">Tente Novamente</h2>
+    </div>
+  );
+};
